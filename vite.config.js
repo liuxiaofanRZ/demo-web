595c6598fe93818bf57eb1da7790cfdea2a40d9a
zxkj_liuxiaofan <1431083935@qq.com>
2022-12-13 17:34:35 +0800

    chore:增加gzip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import viteCompression from 'vite-plugin-compression'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,7 +9,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {},
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    viteCompression({
+      threshold: 512000,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
