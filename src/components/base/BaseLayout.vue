c5384156621978bceca1ddae3a87c4377811167e
zxkj_liuxiaofan <1431083935@qq.com>
2022-12-16 08:26:12 +0800

    refactor:调整文件结构

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -13,5 +13,27 @@ import BaseContent from './BaseContent.vue'
 </script>
 
 <style lang="less">
-@import "../base.less";
+.base-header {
+  z-index: 20;
+  height: 60px;
+  box-shadow: 0 1px 2px 0 var(--cb-color-shadow, rgba(0, 0, 0, 0.1));
+  position: relative;
+}
+.base-container {
+  height: calc(100vh - 60px);
+  display: flex;
+}
+.base-side {
+  overflow-y: auto;
+  height: 100%;
+  width: 220px;
+  z-index: 10;
+  // border-right: 1px solid #c0c6cc;
+}
+.base-content {
+  padding: 20px 20px 20px;
+  flex: 1;
+  height: 100%;
+  overflow-y: auto;
+}
 </style>
