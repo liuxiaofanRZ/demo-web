484d6a6883a50056ffa90c9bf78dc05b59e6def5
zxkj_liuxiaofan <1431083935@qq.com>
2023-03-30 09:00:54 +0800

    refactor:重构路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Error404 from '@/views/system/page/Error404.vue'
 import BaseLayout from '@/components/base/BaseLayout.vue'
-import { addRoutes } from '@/utils/addRoutes'
+import { useMenuStore } from '@/stores/menu'
+import Error404 from '@/views/system/page/Error404.vue'
+import { createRouter, createWebHistory, RouterView } from 'vue-router'
 
+const baseRouteName = 'base'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,7 +11,7 @@ const router = createRouter({
       path: '/',
       redirect: 'Home',
       component: BaseLayout,
-      name: 'base',
+      name: baseRouteName,
       children: [
         // {
         //   path:"/MenuMgt",
@@ -26,10 +27,46 @@ const router = createRouter({
     },
   ],
 })
-// 动态路由添加标志
+
+/* 动态添加路由-开始 */
+const pages = import.meta.glob('/src/views/**/page/*.vue')
+function handleRoute(list, parentName) {
+  list.forEach((menu) => {
+    let route = {
+      name: menu.id, // TODO:在服务端根据path生成该字段,先用id代替
+      path: menu.path,
+      meta: {
+        id: menu.id,
+        pid: menu.pid,
+        title: menu.title,
+        isLeaf: menu.isLeaf,
+      },
+    }
+    if (!menu.path) return
+
+    // 设置路由组件
+    route.component = pages[`/src/views${menu.component}`] || RouterView
+    // 添加路由
+    if (parentName && !menu.isNewPage) {
+      router.addRoute(parentName, route)
+    } else {
+      router.addRoute(route)
+    }
+    // 处理子路由
+    if (menu.children && menu.children.length > 0) {
+      handleRoute(menu.children, route.name)
+    }
+  })
+}
+async function addRoutes() {
+  const menuStore = useMenuStore()
+  await menuStore.getMenu()
+  handleRoute(menuStore.menuList, baseRouteName)
+}
 let dynamicRoutesFlag = false
 router.beforeEach(async (to) => {
   if (dynamicRoutesFlag) {
+    console.log(to);
     return true
   } else {
     await addRoutes(router)
@@ -37,5 +74,6 @@ router.beforeEach(async (to) => {
     return to.fullPath
   }
 })
+/* 动态添加路由-结束 */
 
 export default router
