dbc2e6538ccda7481d472dd3e6a3d62acfbd0df6
zxkj_liuxiaofan <1431083935@qq.com>
2023-03-30 10:53:57 +0800

    feat:增加tmp页

---
+++ src/views/page/Tmp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ state }}</div>
+</template>
+<script setup>
+import { useAsyncState } from '@vueuse/core'
+import { getMenuTree } from '@/api'
+const { state, isReady, isLoading } = useAsyncState(getMenuTree)
+</script>
+<style></style>
